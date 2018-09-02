2bc052acc143159ffa2439967949bdb1ae61220d
Caroline Courtney <caroline@carolinecourtney.com>
2018-09-02 13:14:48 -0400

    DRY refactor to make random images at the start, too

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDiceImages()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -29,6 +30,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         randomDiceIndex1 = Int.random(in: 0 ... 5)
         randomDiceIndex2 = Int.random(in: 0 ... 5)
         
